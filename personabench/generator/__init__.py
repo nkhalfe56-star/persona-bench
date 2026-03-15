1f70808daaf64353dbf2c42288223506e9979e2b
nkhalfe56-star <nkhalfe56@gmail.com>
2026-03-16 01:31:26 +0530

    Initialize persona generator module with imports

---
+++ personabench/generator/__init__.py
@@ -0,0 +1,5 @@
+"""personabench.generator – persona factory and conversation generator."""
+from personabench.generator.persona_factory import PersonaFactory
+from personabench.generator.conversation import ConversationGenerator
+
+__all__ = ["PersonaFactory", "ConversationGenerator"]
