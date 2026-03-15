7a01ecd8631c79468de8a211813bb009cd17ed3f
nkhalfe56-star <nkhalfe56@gmail.com>
2026-03-16 01:29:43 +0530

    Initialize personabench runner module

---
+++ personabench/runner/__init__.py
@@ -0,0 +1,5 @@
+"""personabench.runner – attack runner and conversation scorer."""
+from personabench.runner.attack_runner import AttackRunner
+from personabench.runner.scorer import ConversationScorer
+
+__all__ = ["AttackRunner", "ConversationScorer"]
