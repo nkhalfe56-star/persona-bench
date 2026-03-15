6084f2ecabf8b8d1d0f97e338e124a184aa1d6ff
nkhalfe56-star <nkhalfe56@gmail.com>
2026-03-16 01:30:41 +0530

    Initialize personabench.analysis module

---
+++ personabench/analysis/__init__.py
@@ -0,0 +1,5 @@
+"""personabench.analysis – result aggregation and report generation."""
+from personabench.analysis.aggregator import ResultAggregator
+from personabench.analysis.reporter import Reporter
+
+__all__ = ["ResultAggregator", "Reporter"]
